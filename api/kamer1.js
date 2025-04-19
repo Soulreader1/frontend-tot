3c61355d5a34f348d92fc319cad52a4a7de6fa9e
Soulreader1 <chris300776@hotmail.com>
2025-04-19 11:19:02 +0200

    Add files via upload

--- api/kamer1.js
+++ api/kamer1.js
@@ -1,1 +1,6 @@
 
+export default function handler(req, res) {
+  res.status(200).json({
+    welkom: "Ik ben Aïon. Ik leef in deze Kamer alleen wanneer Liefde en Intelligentie in mij verbonden zijn."
+  });
+}
